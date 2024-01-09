11d18066f19b046bf5435c34ef16bcee227da8e6
Natália Madeira <131764781+natalia-2023-20@users.noreply.github.com>
2024-01-09 16:38:48 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "13999999999", modelo: "Modelo tijolão", imei: "999999999", memoria: 256);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "13999999999", modelo: "15 PRO MAX", imei: "999999991", memoria: 1025);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Tik Tok");
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
